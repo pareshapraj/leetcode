481da274dea2419b6faaee0fdbb19bca33c77ed7
Paresh Apraj <72244369+pareshapraj@users.noreply.github.com>
2024-01-09 22:10:51 +0530

    Time: 2 ms (87.5%), Space: 45 MB (8.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,16 @@
 class Solution {
+
+    // imp qestion needs to do it properly
+    
     public int[] twoSum(int[] A, int k) {
+
         HashMap<Integer, Integer> map = new HashMap<>();
+        
+        
         for(int i = 0, a;; i++){
+        
             if(map.containsKey(A[i]))
+        
                 return new int[]{i, map.get(A[i])};
             map.put(k - A[i], i);
         }
