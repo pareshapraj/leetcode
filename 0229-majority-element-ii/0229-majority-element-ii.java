3fa85385feafacdba2ef4436cbaa6eccd6b99e56
Paresh Apraj <72244369+pareshapraj@users.noreply.github.com>
2024-03-15 15:42:25 +0530

    Time: 3 ms (75.11%), Space: 48.3 MB (19.31%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,23 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-        ArrayList<Integer> list=new ArrayList<>();
-        int n= arr.length;
+        //Optimal Solution
+        //Boyers Moores voting algorithm
+
+        int n=arr.length;
+
+        int count1=0,count2=0;
+        int element1= Integer.MIN_VALUE;
+        int element2= Integer.MIN_VALUE;
+
         for(int i=0;i<n;i++){
-            if(list.size() == 0 || list.get(0) != arr[i]){
-                int count=0;
-                for(int j=0;j<n;j++){
-                    if(arr[j] == arr[i]){
-                        count++;
-                    }
-                }
-                if(count > (n/3)){
-                    list.add(arr[i]);
-                }
-            } 
-            if(list.size()== 2){
-                break;
+            if(count1==0 && element2 != arr[i]){
+                count1=1;
+                element1 = arr[i];
+            }else if(count2 == 0 && element1 != arr[i]){
+                count2= 1;
+                element2 = arr[i];
+            }else if(arr[i]== element1){
+                count1++;
+            }else if(arr[i] == element2){
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
         }
+
+        ArrayList<Integer> list= new ArrayList<>();
+        count1=0; count2=0;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]== element1) count1++;
+            if(arr[i] == element2) count2++;
+        }
+
+        int mini =(int)(n/3)+1;
+        if(count1 >= mini) list.add(element1);
+        if(count2 >= mini) list.add(element2);
 
         return list;
     }
